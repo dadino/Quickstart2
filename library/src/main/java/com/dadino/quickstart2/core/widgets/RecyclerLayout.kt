14b4c68ee153a0916f30d0bbbf54343ed2096b14
David Corsalini <corsalini.david@gmail.com>
2024-08-22 09:55:42 +0200

    - v1.2.2 - add: Sonatype token

--- library/src/main/java/com/dadino/quickstart2/core/widgets/RecyclerLayout.kt
+++ library/src/main/java/com/dadino/quickstart2/core/widgets/RecyclerLayout.kt
@@ -11,91 +11,95 @@ import com.dadino.quickstart2.core.adapters.BaseAdapter
 import com.dadino.quickstart2.core.utils.Colors
 import com.dadino.quickstart2.core.utils.goneIf
 
-open class RecyclerLayout<T : BaseAdapter<*, *>, E : RecyclerView.LayoutManager> : SwipeRefreshLayout {
+open class RecyclerLayout<T : BaseAdapter<*, *>, E : RecyclerView.LayoutManager> :
+    SwipeRefreshLayout {
 
-	private val mList: RecyclerView by lazy { findViewById<RecyclerView>(R.id.list) }
-	private val mEmptyText: TextView by lazy { findViewById<TextView>(R.id.empty_text) }
-	protected var mAdapter: T? = null
-	protected lateinit var mLayoutManager: E
-	private var mLoading: Boolean = false
+    private val mList: RecyclerView by lazy { findViewById<RecyclerView>(R.id.list) }
+    private val mEmptyText: TextView by lazy { findViewById<TextView>(R.id.empty_text) }
+    protected var mAdapter: T? = null
+    protected lateinit var mLayoutManager: E
+    private var mLoading: Boolean = false
+    private var mEmptyIsInstruction: Boolean = false
 
-	constructor(context: Context) : super(context) {
-		init()
-	}
+    constructor(context: Context) : super(context) {
+        init()
+    }
 
-	constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-		init()
-	}
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
 
-	private fun init() {
-		inflate(context, R.layout.view_recycler_layout, this)
+    private fun init() {
+        inflate(context, R.layout.view_recycler_layout, this)
 
-		setColorSchemeColors(Colors.getPrimaryColor(context),
-				Colors.getAccentColor(context),
-				Colors.getPrimaryDarkColor(context))
-		clipToPadding = false
-		initialize()
-	}
+        setColorSchemeColors(
+            Colors.getPrimaryColor(context),
+            Colors.getAccentColor(context),
+            Colors.getPrimaryDarkColor(context)
+        )
+        clipToPadding = false
+        initialize()
+    }
 
-	protected fun initialize() {}
+    protected fun initialize() {}
 
-	fun setListLoading(loading: Boolean) {
-		this.mLoading = loading
-		post {
-			isRefreshing = mLoading
-			updateEmptyTextVisibility()
-		}
-	}
+    fun setListLoading(loading: Boolean) {
+        this.mLoading = loading
+        post {
+            isRefreshing = mLoading
+            updateEmptyTextVisibility()
+        }
+    }
 
-	fun updateLoadingState() {
-		setListLoading(mLoading)
-	}
+    fun updateLoadingState() {
+        setListLoading(mLoading)
+    }
 
-	private fun updateEmptyTextVisibility() {
-		val emptyAdapter = mAdapter == null || mAdapter!!.getItemCount() == 0
-		val shouldBeGone = emptyAdapter.not() || mLoading
-		mEmptyText.goneIf(shouldBeGone)
-	}
+    private fun updateEmptyTextVisibility() {
+        val emptyAdapter = mAdapter == null || mAdapter!!.getItemCount() == 0
+        val shouldBeGone = mEmptyIsInstruction.not() && (emptyAdapter.not() || mLoading)
+        mEmptyText.goneIf(shouldBeGone)
+    }
 
-	fun setEmptyText(text: String) {
-		mEmptyText.text = text
-		updateEmptyTextVisibility()
-	}
+    fun setEmptyText(text: String) {
+        mEmptyText.text = text
+        updateEmptyTextVisibility()
+    }
 
-	fun setEmptyText(@StringRes text: Int) {
-		mEmptyText.setText(text)
-		updateEmptyTextVisibility()
-	}
+    fun setEmptyText(@StringRes text: Int) {
+        mEmptyText.setText(text)
+        updateEmptyTextVisibility()
+    }
 
-	var adapter: T?
-		get() = mAdapter
-		set(adapter) {
-			if (isInEditMode) return
-			this.mAdapter = adapter
-			mAdapter?.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
-				override fun onChanged() {
-					super.onChanged()
-					updateLoadingState()
-				}
-			})
-			mList.adapter = mAdapter
-			updateLoadingState()
-		}
+    var adapter: T?
+        get() = mAdapter
+        set(adapter) {
+            if (isInEditMode) return
+            this.mAdapter = adapter
+            mAdapter?.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+                override fun onChanged() {
+                    super.onChanged()
+                    updateLoadingState()
+                }
+            })
+            mList.adapter = mAdapter
+            updateLoadingState()
+        }
 
-	fun setLayoutManager(layoutManager: E) {
-		this.mLayoutManager = layoutManager
-		mList.layoutManager = layoutManager
-	}
+    fun setLayoutManager(layoutManager: E) {
+        this.mLayoutManager = layoutManager
+        mList.layoutManager = layoutManager
+    }
 
-	fun setBottomPadding(paddingInPixel: Int) {
-		mList.setPadding(paddingLeft, paddingTop, paddingRight, paddingInPixel)
-	}
+    fun setBottomPadding(paddingInPixel: Int) {
+        mList.setPadding(paddingLeft, paddingTop, paddingRight, paddingInPixel)
+    }
 
-	fun addItemDecoration(itemDecorator: RecyclerView.ItemDecoration) {
-		mList.addItemDecoration(itemDecorator)
-	}
+    fun addItemDecoration(itemDecorator: RecyclerView.ItemDecoration) {
+        mList.addItemDecoration(itemDecorator)
+    }
 
-	fun setHasFixedSize(hasFixedSize: Boolean) {
-		mList.setHasFixedSize(hasFixedSize)
-	}
+    fun setHasFixedSize(hasFixedSize: Boolean) {
+        mList.setHasFixedSize(hasFixedSize)
+    }
 }
